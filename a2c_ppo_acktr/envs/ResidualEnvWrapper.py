a0876efa941066a8486f90ffae2cd3305867a01f
Harry Uglow <hu115@ic.ac.uk>
2019-04-30 18:37:41 +0100

    Added ResidualVecEnvWrapper.

---
+++ a2c_ppo_acktr/envs/ResidualEnvWrapper.py
@@ -0,0 +1,39 @@
+import numpy as np
+import torch
+from baselines.common.vec_env import VecEnvWrapper
+
+
+class ResidualVecEnvWrapper(VecEnvWrapper):
+    def __init__(self, venv, initial_policy, ob_rms, clipob=10., epsilon=1e-8):
+        super(ResidualVecEnvWrapper, self).__init__(venv)
+        self.ip = initial_policy
+        self.ob_rms = ob_rms
+        self.clipob = clipob
+        self.epsilon = epsilon
+        self.last_obs = None
+        self.rnn_hxs = torch.zeros(venv.num_envs, initial_policy.recurrent_hidden_state_size)
+        self.masks = torch.zeros(venv.num_envs, 1)
+
+    def normalize_obs(self, obs):
+        if self.ob_rms:
+            obs = np.clip((obs - self.ob_rms.mean) / np.sqrt(self.ob_rms.var + self.epsilon), -self.clipob, self.clipob)
+        return obs
+
+    def step_wait(self):
+        obs, rew, done, info = self.venv.step_wait()
+        self.masks = torch.FloatTensor([[0.0] if done_ else [1.0]
+                                   for done_ in done])
+
+        self.last_obs = self.normalize_obs(obs)
+        pass
+
+    def step_async(self, action):
+        _, ip_action, _, self.rnn_hxs = self.ip.act(self.last_obs, self.rnn_hxs, self.masks,
+                                                    deterministic=True)
+        whole_action = ip_action + action
+        self.venv.step_async(whole_action)
+
+    def reset(self, **kwargs):
+        obs = self.venv.reset(**kwargs)
+        self.last_obs = self.normalize_obs(obs)
+        return obs
--- a2c_ppo_acktr/envs/VrepEnv.py
+++ a2c_ppo_acktr/envs/VrepEnv.py
@@ -1,4 +1,3 @@
-import atexit
 import os
 import platform
 import signal
@@ -64,7 +63,6 @@ class VrepEnv(Env):
         time.sleep(6)
 
         self.cid = vrep.simxStart(host, port_num, True, True, 5000, 5)
-        atexit.register(self.close)
 
     # Function to call a Lua function in V-Rep
     # Read more here: http://www.coppeliarobotics.com/helpFiles/en/remoteApiExtension.htm
@@ -81,7 +79,6 @@ class VrepEnv(Env):
         print("Closing VREP")
         vrep.simxStopSimulation(self.cid, vrep.simx_opmode_blocking)
         vrep.simxFinish(self.cid)
-        atexit.unregister(self.close)
         try:
             pgrp = os.getpgid(self.process.pid)
             os.killpg(pgrp, signal.SIGKILL)
