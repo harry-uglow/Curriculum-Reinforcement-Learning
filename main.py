45cac03f934269947de85b8b3bb246ad86dbb311
Harry Uglow <hu115@ic.ac.uk>
2019-05-09 00:20:22 +0100

    Added 'burn in' by stealing critic from outermost initial policy.

--- main.py
+++ main.py
@@ -59,9 +59,14 @@ def main():
     envs = make_vec_envs(args.env_name, args.seed, args.num_processes, args.gamma, args.log_dir,
                          args.add_timestep, device, False, initial_policies)
 
-    actor_critic = Policy(envs.observation_space.shape, envs.action_space,
-                          base_kwargs={'recurrent': args.recurrent_policy,
-                                       'zero_last_layer': initial_policies is not None})
+    base_kwargs = {'recurrent': args.recurrent_policy,
+                   'zero_last_layer': initial_policies is not None}
+    if initial_policies is not None:
+        base_kwargs['hidden_size'] = initial_policies[0].base._hidden_size
+    actor_critic = Policy(envs.observation_space.shape, envs.action_space, base_kwargs=base_kwargs)
+    if initial_policies is not None:
+        critic_state = initial_policies[0].base.critic.state_dict()
+        actor_critic.base.critic.load_state_dict(critic_state)
     actor_critic.to(device)
 
     if args.algo == 'a2c':
