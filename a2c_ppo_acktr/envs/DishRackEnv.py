55863cd2c0b33134ab5cf75744c609842c263ba3
Harry Uglow <hu115@ic.ac.uk>
2019-05-14 15:36:59 +0100

    Added NonRespondableDREnv for training initial policy.

--- a2c_ppo_acktr/envs/DishRackEnv.py
+++ a2c_ppo_acktr/envs/DishRackEnv.py
@@ -72,7 +72,7 @@ class DishRackEnv(SawyerEnv):
         rew = 0.01 * (rew_dist + rew_ctrl + 0.05 * rew_orientation + 0.1 * rew_collision)
 
         return ob, rew, done, dict(rew_dist=rew_dist, rew_ctrl=rew_ctrl,
-                                   rew_orientation=rew_orientation,)
+                                   rew_orientation=rew_orientation, rew_collision=rew_collision)
 
     def _get_obs(self):
         joint_obs = super(DishRackEnv, self)._get_obs()
@@ -94,3 +94,25 @@ class DishRackEnv(SawyerEnv):
         orientation = catch_errors(vrep.simxGetObjectOrientation(
             self.cid, self.plate_handle, self.target_handle, vrep.simx_opmode_blocking))
         return np.array(orientation[:-1])
+
+
+class NonRespondableDREnv(DishRackEnv):
+
+    def step(self, a):
+        self.target_velocities = a
+        dist = np.linalg.norm(self.get_plate_pos() - self.target_pos)
+        orientation_diff = np.abs(self.get_plate_orientation()).sum()
+
+        self.timestep += 1
+        self.update_sim()
+
+        ob = self._get_obs()
+        done = (self.timestep == self.ep_len)
+
+        rew_dist = - dist
+        rew_ctrl = - np.square(np.abs(self.target_velocities).mean())
+        rew_orientation = - orientation_diff / max(dist, 0.11)  # Radius = 0.11
+        rew = 0.01 * (rew_dist + rew_ctrl + 0.04 * rew_orientation)
+
+        return ob, rew, done, dict(rew_dist=rew_dist, rew_ctrl=rew_ctrl,
+                                   rew_orientation=rew_orientation)
--- a2c_ppo_acktr/envs/envs.py
+++ a2c_ppo_acktr/envs/envs.py
@@ -11,7 +11,7 @@ from baselines.common.vec_env.subproc_vec_env import SubprocVecEnv
 from baselines.common.vec_env.dummy_vec_env import DummyVecEnv
 from baselines.common.vec_env.vec_normalize import VecNormalize as VecNormalize_
 
-from a2c_ppo_acktr.envs.DishRackEnv import DishRackEnv
+from a2c_ppo_acktr.envs.DishRackEnv import DishRackEnv, NonRespondableDREnv
 from a2c_ppo_acktr.envs.DishRackVisEnv import DishRackVisEnv
 from a2c_ppo_acktr.envs.ReachOverWallEnv import ReachOverWallEnv, ROWRandomTargetEnv, ReachNoWallEnv
 from a2c_ppo_acktr.envs.ResidualVecEnvWrapper import ResidualVecEnvWrapper
@@ -34,7 +34,7 @@ except ImportError:
 
 def make_env(env_id, seed, rank, log_dir, add_timestep, allow_early_resets, vis):
     def _thunk():
-        env = DishRackVisEnv(seed, rank, not vis)
+        env = NonRespondableDREnv(seed, rank, not vis)
         if log_dir is not None:
             env = bench.Monitor(env, os.path.join(log_dir, str(rank)),
                                 allow_early_resets=allow_early_resets)
