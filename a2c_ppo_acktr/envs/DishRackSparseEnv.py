Fixed bug in reward calculation for sparse DRE.

a2c_ppo_acktr/envs/DishRackSparseEnv.py
@@ -18,7 +18,7 @@ class DishRackSparseEnv(DishRackEnv):
 
     def step(self, a):
         self.target_velocities = a
-        dist = self.get_plate_dist()
+        dist = np.abs(self.get_plate_dist())
         orientation_diff = np.abs(self.get_plate_orientation())
 
         rew_success = 0.1 if np.all(orientation_diff <= max_rot) and np.all(dist <= max_dist) else 0