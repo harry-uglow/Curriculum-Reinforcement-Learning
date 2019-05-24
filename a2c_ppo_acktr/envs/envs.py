bce21f684727b63f4309b4d172d41f593961aa13
Harry Uglow <hu115@ic.ac.uk>
2019-05-24 11:50:13 +0100

    RIP training going quite well. Beefed up the network to try again.

--- a2c_ppo_acktr/envs/envs.py
+++ a2c_ppo_acktr/envs/envs.py
@@ -35,7 +35,7 @@ except ImportError:
 
 def make_env(env_id, seed, rank, log_dir, add_timestep, allow_early_resets, vis):
     def _thunk():
-        env = DishRackSparseEnv(seed, rank, not vis)
+        env = DRNoWaypointEnv(seed, rank, not vis)
         if log_dir is not None:
             env = bench.Monitor(env, os.path.join(log_dir, str(rank)),
                                 allow_early_resets=allow_early_resets)
