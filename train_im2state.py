4230868438f535f43741067162728e6260456227
Harry Uglow <hu115@ic.ac.uk>
2019-06-01 16:32:20 +0100

    Evaluate trained model at the end of train_im2state.

--- train_im2state.py
+++ train_im2state.py
@@ -11,7 +11,7 @@ from tqdm import tqdm
 from a2c_ppo_acktr.arguments import get_args
 from im2state.model import PoseEstimator
 
-from im2state.utils import normalise_coords
+from im2state.utils import normalise_coords, unnormalise_y
 
 args = get_args()
 
@@ -68,8 +68,8 @@ def main():
         epochs += 1
         losses = []
         for batch_idx in tqdm(range(0, len(train_x), batch_size)):
-            actual_y = net(train_x[batch_idx:batch_idx + batch_size])
-            loss = criterion(actual_y, train_y[batch_idx:batch_idx + batch_size])
+            pred_y = net(train_x[batch_idx:batch_idx + batch_size])
+            loss = criterion(pred_y, train_y[batch_idx:batch_idx + batch_size])
             losses += [loss.item()]
 
             optimizer.zero_grad()
@@ -101,6 +101,22 @@ def main():
             plt.savefig(f'imgs/{args.save_as}.png')
             plt.close(fig)
             print(f"Training epoch {epochs} - validation loss: {test_loss[-1]}")
+
+    print("Finished training")
+    print("Evaluating")
+    net = torch.load(os.path.join(save_path, args.save_as + ".pt"))
+    net.eval()
+    with torch.no_grad():
+        distances = []
+        thetas = []
+        for x, y in zip(test_x, unnormalise_y(test_y.detach().numpy(), low, high)):
+            actual_y = unnormalise_y(net(x.unsqueeze(0)).squeeze().detach().numpy(), low, high)
+            pred_y = y
+            distances += [np.linalg.norm(pred_y[:2] - actual_y[:2])]
+            thetas += [pred_y[2] - actual_y[2]]
+        print(f"Mean distance error: {(1000 * sum(distances) / len(distances))}mm")
+        print(f"Mean rotational error: {(sum(thetas) / len(thetas))} radians")
+        print(f"Final test loss: {criterion(net(test_x), test_y).item()}")
 
 
 if __name__ == "__main__":
