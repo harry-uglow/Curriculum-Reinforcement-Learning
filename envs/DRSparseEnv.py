1f3fa0eee2d0e96750fe7494e14d2b1e91cf8365
Harry Uglow <hu115@ic.ac.uk>
2019-06-07 17:18:15 +0100

    Amended reward function in DRSparseEnv to better reward closer targets. Updated dish_rack_pr_18.

--- envs/DRSparseEnv.py
+++ envs/DRSparseEnv.py
@@ -6,7 +6,8 @@ from envs.DishRackEnv import DishRackEnv
 np.set_printoptions(precision=2, linewidth=200)  # DEBUG
 dir_path = os.getcwd()
 
-max_dist = 0.015  # 1.5cm
+max_displacement = 0.015  # 1.5cm
+max_dist = np.linalg.norm([max_displacement]*3)
 max_rot = 0.1  # ~5.7 deg
 
 
@@ -14,10 +15,13 @@ class DRSparseEnv(DishRackEnv):
 
     def step(self, a):
         self.target_velocities = a
-        dist = np.abs(self.get_vector(self.target_handle, self.plate_handle))
+        displacement = np.abs(self.get_vector(self.target_handle, self.plate_handle))
         orientation_diff = np.abs(self.get_plate_orientation())
 
-        rew_success = 0.1 if np.all(orientation_diff <= max_rot) and np.all(dist <= max_dist) else 0
+        rew_success = 0.1 if np.all(orientation_diff <= max_rot) and \
+                             np.all(displacement <= max_displacement) else 0
+        dist = np.linalg.norm(displacement)
+        rew = rew_success * (1 - dist / max_dist)
 
         self.timestep += 1
         self.update_sim()
@@ -25,7 +29,7 @@ class DRSparseEnv(DishRackEnv):
         ob = self._get_obs()
         done = (self.timestep == self.ep_len)
 
-        return ob, rew_success, done, dict(rew_success=rew_success)
+        return ob, rew , done, dict(rew_success=rew_success)
 
     def __init__(self, scene_path, *args):
         self.scene_path = scene_path
