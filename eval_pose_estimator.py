Fixed batch normalisation in im2state/model

eval_pose_estimator.py
@@ -24,7 +24,7 @@ def eval_pose_estimator(load_path, device, x, y, low, high):
         distances = []
         thetas = []
         for x_, y_ in tqdm(zip(x, y)):
-            actual_y = unnormalise_y(net(x_.unsqueeze(0)).squeeze().cpu().numpy(), low, high)
+            actual_y = unnormalise_y(net(x_.unsqueeze(0)), low, high).squeeze().cpu().numpy()
             pred_y = y_
             distances += [np.linalg.norm(pred_y[:2] - actual_y[:2])]
             thetas += [np.abs(pred_y[2] - actual_y[2])]
@@ -46,4 +46,4 @@ if __name__ == '__main__':
 
     x = torch.Tensor(images[p][:args.num_examples])
     y = torch.Tensor(positions[p][:args.num_examples])
-    eval_pose_estimator(load_path, device, x, y, low, high)
+    eval_pose_estimator(load_path, device, x, y, torch.Tensor(low), torch.Tensor(high))

im2state/model.py
@@ -69,6 +69,7 @@ class PoseEstimator(nn.Module):
 
     def forward(self, x):
         # Normalise inputs
-        x = self.normalize(x / 255.0)
+        for i in range(x.size(0)):
+            x[i] = self.normalize(x[i] / 255.0)
         x = self.main(x)
         return x

train_im2state.py
@@ -31,6 +31,8 @@ def main():
     images, abs_positions, rel_positions, low, high = torch.load(
         os.path.join(args.load_dir, args.env_name + ".pt"))
     print("Loaded")
+    low = torch.Tensor(low)
+    high = torch.Tensor(high)
     images = np.transpose([np.array(img) for img in images], (0, 3, 1, 2))
 
     save_path = os.path.join('trained_models', 'im2state')
@@ -84,7 +86,9 @@ def main():
 
         train_loss += [np.mean(losses)]
         with torch.no_grad():
-            test_loss += [criterion(net(test_x), test_y).item()]
+            test_output = net(test_x)
+            test_output = test_output if args.rel else unnormalise_y(test_output, low, high)
+            test_loss += [criterion(test_output, test_y).item()]
         if test_loss[-1] < min_test_loss:
             updates_with_no_improvement = 0
             min_test_loss = test_loss[-1]