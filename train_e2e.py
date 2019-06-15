b152a924f06fcd067d42900e58d7b979ce75351f
Harry Uglow <hu115@ic.ac.uk>
2019-06-15 11:27:33 +0100

    Merge.

--- train_e2e.py
+++ train_e2e.py
@@ -6,6 +6,7 @@ import numpy as np
 import torch
 from torch import optim, nn
 import matplotlib.pyplot as plt
+from torchvision import transforms
 from tqdm import tqdm
 
 from a2c_ppo_acktr.arguments import get_args
@@ -28,12 +29,12 @@ def main():
     torch.set_num_threads(1)
     device = torch.device(f"cuda:{args.device_num}" if args.cuda else "cpu")
 
-    images, abs_positions, rel_positions, low, high, _, _ = torch.load(
+    images, _, _, _, _, joint_angles, actions = torch.load(
         os.path.join(args.load_dir, args.env_name + ".pt"))
     print("Loaded")
-    print(len(images))
-    low = torch.Tensor(low).to(device)
-    high = torch.Tensor(high).to(device)
+    low = torch.Tensor([-0.3] * 7).to(device)
+    high = torch.Tensor([0.3] * 7).to(device)
+    images = np.transpose([np.array(img) for img in images], (0, 3, 1, 2))
 
     save_path = os.path.join('trained_models', 'im2state')
     try:
@@ -41,11 +42,9 @@ def main():
     except OSError:
         pass
 
-    positions = rel_positions if args.rel else abs_positions
-
     pretrained_name = 'vgg16_4out.pt' if args.rel else 'vgg16_3out.pt'
-    net = PoseEstimator(3, positions.shape[1])
-    # net.load_state_dict(torch.load(os.path.join('trained_models/pretrained/', pretrained_name)))
+
+    net = PoseEstimator(3, actions.shape[1])
     net = net.to(device)
 
     optimizer = optim.Adam(net.parameters(), lr=args.lr)
@@ -54,20 +53,20 @@ def main():
     np_random = np.random.RandomState()
     np_random.seed(1053831)
     p = np_random.permutation(len(images))
-    positions = positions[p]
+    images = images[p]
+    joint_angles = joint_angles[p]
+    actions = actions[p]
 
     num_test_examples = images.shape[0] // 10
     num_train_examples = images.shape[0] - num_test_examples
     batch_size = 100
 
-    train_indices = p[:num_test_examples]
-    test_indices = p[num_test_examples:]
-    test_x = np.array((batch_size, 3, 128, 128))
-    for i, idx in enumerate(test_indices):
-        test_x[i] = np.transpose(images[i], (2, 0, 1))
-
-    test_y = positions[:num_test_examples]
-    train_y = positions[num_test_examples:]
+    test_x = images[:num_test_examples]
+    aux_test_x = joint_angles[:num_test_examples]
+    test_y = actions[:num_test_examples]
+    train_x = images[num_test_examples:]
+    aux_train_x = joint_angles[num_test_examples:]
+    train_y = actions[num_test_examples:]
 
     train_loss_x_axis = []
     train_loss = []
@@ -80,12 +79,10 @@ def main():
     epochs = 0
     while updates_with_no_improvement < 5:
         for batch_idx in tqdm(range(0, num_train_examples, batch_size)):
-            indices = train_indices[batch_idx:batch_idx + batch_size]
-            train_x = np.array((batch_size, 3, 128, 128))
-            for i, idx in enumerate(indices):
-                train_x[i] = np.transpose(images[i], (2, 0, 1))
-
-            output = net.predict(torch.Tensor(train_x).to(device))
+            output = net.predict(
+                torch.Tensor(train_x[batch_idx:batch_idx + batch_size]).to(device),
+                torch.Tensor(aux_train_x[batch_idx:batch_idx + batch_size]).to(device),
+            )
             pred_y = output if args.rel else unnormalise_y(output, low, high)
             loss = criterion(pred_y,
                              torch.Tensor(train_y[batch_idx:batch_idx + batch_size]).to(device))
@@ -100,10 +97,14 @@ def main():
         loss = 0
         with torch.no_grad():
             for batch_idx in tqdm(range(0, num_test_examples, batch_size)):
-                test_output = net.predict(torch.Tensor(test_x[batch_idx:batch_idx + batch_size]).to(device))
+                test_output = net.predict(
+                    torch.Tensor(test_x[batch_idx:batch_idx + batch_size]).to(device),
+                    torch.Tensor(aux_test_x[batch_idx:batch_idx + batch_size]).to(device),
+                )
                 test_output = test_output if args.rel else unnormalise_y(test_output, low, high)
-                loss += criterion(test_output,
-                             torch.Tensor(test_y[batch_idx:batch_idx + batch_size]).to(device)).item()
+                loss += criterion(
+                    test_output,
+                    torch.Tensor(test_y[batch_idx:batch_idx + batch_size]).to(device)).item()
 
             test_loss += [loss / (num_test_examples // batch_size)]
         if test_loss[-1] < min_test_loss:
